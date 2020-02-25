fb79206a92cf4a6529d523f57d21a562e5ee51cb
Heredin <herevel77@gmail.com>
2020-02-25 13:49:47 -0500

    Subiendo estructura de rutas y diseño de login v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
+// Declarar tus rutas aca
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home', loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule) },
+{path: 'login', component: LoginComponent},
+];
 
-const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [ LoginComponent, SidebarComponent, FooterComponent, HeaderComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    HeaderComponent,
-    FooterComponent,
-    SidebarComponent
+    routingComponents,
   ],
   imports: [
     BrowserModule,
